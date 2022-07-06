fc9220db5c7b054cf237f5983b9f4cc8aed13c44
jsn_ <jsn_@naver.com>
2022-07-06 15:55:37 +0900

    up

--- reiweb/docapi/views.py
+++ reiweb/docapi/views.py
@@ -1,6 +1,29 @@
+from django.contrib.staticfiles import finders
 from django.shortcuts import render
 
-# Create your views here.
+# importing the necessary libraries
+import os
+from io import BytesIO, StringIO
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.conf import settings
+
+from xhtml2pdf import pisa
+from django.views.generic import View
+
+
+# defining the function to convert an HTML file to a PDF file
+def html_to_pdf(template_src, context_dict=None):
+    if context_dict is None: context_dict = {}
+
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    result = BytesIO()
+
+    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result, debug=1, encoding='UTF-8')
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
 
 
 def main(request):
@@ -12,15 +35,24 @@ def main(request):
         rent = {'part': '702호', 'area': '27.85'}
         pay = {'sum': '이억삼천만원', 'down': '이천삼백만원', 'middle': '', 'balance': '이억칠백만원', 'rent': ''}
         day = {'middle': '', 'balance': '2022년 5월 9일', 'rent': ''}
-        clause = [{'num': '제 1조 ', 'title': '[ 목적 ]', 'content': '위 부동산의 임대차에 한하여 임대인과 임차인은 합의에 의하여 임차보증금 및 차임을 아래와 같이 지불하기로 한다.'},
-                  {'num': '제 2조 ', 'title': '[ 존속기간 ]', 'content': '임대인은 위 부동산을 임대차 목적대로 사용수익 할 수 있는 상태로        년       월       일까지 임차인에게 인도하며, 임대차 기간은 인도일로부터        년       월       일까지 (       개월)로 한다.'},
-                  {'num': '제 3조 ', 'title': '[ 용도변경 및 전대 등 ]', 'content': '임차인은 임대인의 동의없이 위 부동산의 용도나 구조를 변경하거나 전대, 임차권 양도 또는 담보 제공을 하지 못하며 임대차 목적 이외의 용도로 사용할 수 없다.'},
-                  {'num': '제 4조 ', 'title': '[ 계약의 해지 ]', 'content': '임차인의 차임 연체액이 (         )기의 차임액에 달하거나, 제3조를 위반하였을 때 임대인은 즉시 본 계약을 해지 할 수 있다.'},
-                  {'num': '제 5조 ', 'title': '[ 계약의 종료 ]', 'content': '임대차계약이 종료된 경우에 임차인은 위 부동산을 원상으로 회복하여 임대인에게 반환한다. 이러한 경우 임대인은 보증금을 임차인에게 반환하고, 연체 임대료 또는 손해배상금이 있을 때는 이들을 제하고 그 잔액을 반환한다.'},
-                  {'num': '제 6조 ', 'title': '[ 계약의 해제 ]', 'content': '임차인이 임대인에게 중도금(중도금이 없을 때는 잔금)을 지불하기 전까지 임대인은 계약금의 배액을 상환 하고, 임차인은 계약금을 포기하고 이 계약을 해제할 수 있다.'},
-                  {'num': '제 7조 ', 'title': '[ 채무불이행과 손해배상의 예정 ]', 'content': '임대인 또는 임차인은 본 계약상의 내용에 대하여 불이행이 있을 경우 그 상대방은 불이행 한 자에 대하여 서면으로 최고하고 계약을 해제 할 수 있다. 이 경우 계약 당사자는 계약해제에 따른 손해배상을 각각 상대방에게 청구할 수 있으며, 손해배상에 대하여 별도의 약정이 없는 한 계약금을 손해배상의 기준으로 본다.'},
-                  {'num': '제 8조 ', 'title': '[ 중개보수 ]', 'content': '개업공인중개사는 임대인 또는 임차인의 본 계약 불이행에 대하여 책임을 지지 않는다. 또한 중개보수는 본 계약 체결에 따라 계약 당사자 쌍방이 각각 지불하며, 개업공인중개사의 고의나 과실 없이 본 계약이 무효, 취소 또는 해제되어도 중개보수는 지급한다. 공동중개인 경우에 임대인과 임차인은 자신이 중개 의뢰한 개업공인중개사에게 각각 중개보수를 지급한다'},
-                  {'num': '제 9조 ', 'title': '[ 중개대상물 확인설명서 교부 등 ]', 'content': '개업공인중개사는 중개대상물 확인설명서를 작성하고 업무보증관계증서(공제증서 등) 사본을 첨부하여 계약체결과 동시에 거래당사자 쌍방에게 교부한다. (교부일자 :        년      월      일)'},
+        clause = [{'num': '제 1조 ', 'title': '[ 목적 ]',
+                   'content': '위 부동산의 임대차에 한하여 임대인과 임차인은 합의에 의하여 임차보증금 및 차임을 아래와 같이 지불하기로 한다.'},
+                  {'num': '제 2조 ', 'title': '[ 존속기간 ]',
+                   'content': '임대인은 위 부동산을 임대차 목적대로 사용수익 할 수 있는 상태로        년       월       일까지 임차인에게 인도하며, 임대차 기간은 인도일로부터        년       월       일까지 (       개월)로 한다.'},
+                  {'num': '제 3조 ', 'title': '[ 용도변경 및 전대 등 ]',
+                   'content': '임차인은 임대인의 동의없이 위 부동산의 용도나 구조를 변경하거나 전대, 임차권 양도 또는 담보 제공을 하지 못하며 임대차 목적 이외의 용도로 사용할 수 없다.'},
+                  {'num': '제 4조 ', 'title': '[ 계약의 해지 ]',
+                   'content': '임차인의 차임 연체액이 (         )기의 차임액에 달하거나, 제3조를 위반하였을 때 임대인은 즉시 본 계약을 해지 할 수 있다.'},
+                  {'num': '제 5조 ', 'title': '[ 계약의 종료 ]',
+                   'content': '임대차계약이 종료된 경우에 임차인은 위 부동산을 원상으로 회복하여 임대인에게 반환한다. 이러한 경우 임대인은 보증금을 임차인에게 반환하고, 연체 임대료 또는 손해배상금이 있을 때는 이들을 제하고 그 잔액을 반환한다.'},
+                  {'num': '제 6조 ', 'title': '[ 계약의 해제 ]',
+                   'content': '임차인이 임대인에게 중도금(중도금이 없을 때는 잔금)을 지불하기 전까지 임대인은 계약금의 배액을 상환 하고, 임차인은 계약금을 포기하고 이 계약을 해제할 수 있다.'},
+                  {'num': '제 7조 ', 'title': '[ 채무불이행과 손해배상의 예정 ]',
+                   'content': '임대인 또는 임차인은 본 계약상의 내용에 대하여 불이행이 있을 경우 그 상대방은 불이행 한 자에 대하여 서면으로 최고하고 계약을 해제 할 수 있다. 이 경우 계약 당사자는 계약해제에 따른 손해배상을 각각 상대방에게 청구할 수 있으며, 손해배상에 대하여 별도의 약정이 없는 한 계약금을 손해배상의 기준으로 본다.'},
+                  {'num': '제 8조 ', 'title': '[ 중개보수 ]',
+                   'content': '개업공인중개사는 임대인 또는 임차인의 본 계약 불이행에 대하여 책임을 지지 않는다. 또한 중개보수는 본 계약 체결에 따라 계약 당사자 쌍방이 각각 지불하며, 개업공인중개사의 고의나 과실 없이 본 계약이 무효, 취소 또는 해제되어도 중개보수는 지급한다. 공동중개인 경우에 임대인과 임차인은 자신이 중개 의뢰한 개업공인중개사에게 각각 중개보수를 지급한다'},
+                  {'num': '제 9조 ', 'title': '[ 중개대상물 확인설명서 교부 등 ]',
+                   'content': '개업공인중개사는 중개대상물 확인설명서를 작성하고 업무보증관계증서(공제증서 등) 사본을 첨부하여 계약체결과 동시에 거래당사자 쌍방에게 교부한다. (교부일자 :        년      월      일)'},
                   ]
         agrs = """
         <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
@@ -47,5 +79,8 @@ p, li { white-space: pre-wrap; }
 
         context = {'address': address, 'land': land, 'bld': bld, 'rent': rent, 'pay': pay, 'day': day, 'clause': clause,
                    'agrs': agrs, 'contractor': contractor, 'agent': agent}
+
+        pdf = html_to_pdf(template, context)
         return render(request, template, context)
+        # return HttpResponse(pdf, content_type='application/pdf')
     return render(request, 'index.html')
