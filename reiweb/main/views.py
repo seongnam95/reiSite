294398b4a98b85cee5a3a9aaa166f1ba3d1e193d
jsn_ <jsn_@naver.com>
2022-07-20 19:44:42 +0900

    up

--- reiweb/main/views.py
+++ reiweb/main/views.py
@@ -15,8 +15,9 @@ def contract(request):
 
 
 def temp_data():
+    contract_kind = ['일반 계약서', '표준임대차 계약서', '분양권']
     prop_type = ['아파트', '단독주택', '다세대주택', '다가구주택']
-    prop = {'type': prop_type}
+    prop = {'kind': contract_kind, 'type': prop_type}
 
     context = {'prop': prop}
     return context
