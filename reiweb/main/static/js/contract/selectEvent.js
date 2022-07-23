0d09f09362a6e6828f4342867bd157af55465903
seongnam95 <jsn_@naver.com>
2022-07-23 22:46:42 +0900

    up

--- reiweb/main/static/js/contract/selectEvent.js
+++ reiweb/main/static/js/contract/selectEvent.js
@@ -1,8 +1,77 @@
+$(document).ready(function(){
+      $("#txt_deposit").blur(function(){
+      });
 
-$(document).ready(function() {
-    $("#opt_type option:selected").change(function () {
-        month = $('#opt_type option:selected').val();
-        $('.form').submit();
+    // 금액 입력 시
+    $("#txt_deposit").on("change keyup paste", function() {
+        var str = $("#txt_deposit").val();
+        console.log(str);
+        str = str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,')
+        $("#txt_deposit").val(str);
+        var convVal = viewKorean(str);
+        $("#hint_deposit").text(convVal);
     });
 
+    // 셀렉트 Box
+    var selectTarget = $('.selectbox select');
+    selectTarget.on('blur', function(){
+        $(this).parent().removeClass('focus');
+    });
+
+    selectTarget.change(function(){
+        const target = $(this);
+
+        var target_val = $(this).children('option:selected').text();
+        target.siblings('label').text(target_val);
+
+        if (target.prop("id") == $("#opt_type").prop("id")){
+            if (target_val != "계약서 선택") {
+                $.ajax({
+                    url: "/main/contract/",
+                    type: "GET",
+                    data: {'data': target_val},
+                    dataType: "html",
+                    success:function(){ },
+                    error: function(request, status, error){
+                      alert('서버와의 연결에 실패하였습니다.');
+                    }
+                });
+            }
+        }
+    });
 });
+
+// 금액표기 한글 변환
+function viewKorean(num) {
+    var hanA = new Array("","일","이","삼","사","오","육","칠","팔","구","십");
+    var danA = new Array("","십","백","천","","십","백","천","","십","백","천","","십","백","천");
+    var result = "";
+    for(i=0; i<num.length; i++) {
+        str = "";
+        han = hanA[num.charAt(num.length-(i+1))];
+        if(han != "") str += han+danA[i];
+        if(i == 4) str += "만";
+        if(i == 8) str += "억";
+        if(i == 12) str += "조";
+        result = str + result;
+    }
+    if(num != 0)
+        result = result + "원";
+    return result ;
+}
+
+function commas(t) {
+	// 콤마 빼고
+	var x = t.value;
+	x = x.replace(/,/gi, '');
+
+        // 숫자 정규식 확인
+	var regexp = /^[0-9]*$/;
+	if(!regexp.test(x)){
+		$(t).val("");
+		alert("숫자만 입력 가능합니다.");
+	} else {
+		x = x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		$(t).val(x);
+	}
+}
--- reiweb/main/views.py
+++ reiweb/main/views.py
@@ -9,16 +9,16 @@ def main(request):
 
 
 def contract(request):
-    print('a')
     template = 'contract/basic.html'
-    context = temp_data()
+    context = temp_data(request.GET.get('data'))
+    print(render(request, template, context))
     return render(request, template, context)
 
 
-def temp_data():
+def temp_data(data=None):
     contract_kind = ['일반 계약서', '표준임대차 계약서', '분양권']
     prop_type = ['아파트', '단독주택', '다세대주택', '다가구주택']
     prop = {'kind': contract_kind, 'type': prop_type}
 
-    context = {'prop': prop}
+    context = {'data': data, 'prop': prop}
     return context
